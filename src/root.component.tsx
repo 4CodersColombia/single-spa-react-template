f046b65a5e20e6f9dbdfb301948fa5aa5d33882c
alvarojbucaro <53539479+alvarojbucaro@users.noreply.github.com>
2021-08-13 13:21:37 -0500

    XCH-20-TEMPLATE-REACT-SINGLE-SPA (#1)
    * XCH-20-TEMPLATE-REACT-SINGLE-SPA
    
    * test component
    
    * Create set-public-path.ts

---
+++ src/root.component.tsx
@@ -0,0 +1,7 @@
+import Test from "./components/test/test";
+export default function Root(props) {
+  const test = Test({
+    name: "funcionaaaa modularmenteeeee React!!!!!!!!!!!!!",
+  });
+  return test;
+}
