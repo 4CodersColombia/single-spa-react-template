c0564e31daaaff64a8fec9eb66ec462dac5bd9f1
alvarojbucaro <53539479+alvarojbucaro@users.noreply.github.com>
2021-09-06 17:32:54 -0500

    XCH-120-i18N (#2)
    * XCH-120-i18N
    
    * fix sonar cloud

---
+++ src/i18N/ApiTranslator.ts
@@ -0,0 +1,22 @@
+import i18next from "i18next";
+
+const getResourcesLanguages = (resources: { [key: string]: Object }) => {
+  const keysLanguages = Object.keys(resources);
+  const languages = keysLanguages.map((item) => {
+    return { [item]: { translation: resources[item] } };
+  });
+  return languages.reduce((acum, current) => {
+    return {
+      ...acum,
+      ...current,
+    };
+  }, {});
+};
+export const postTranslate = async (resources: { [key: string]: Object }) => {
+  console.log(getResourcesLanguages(resources));
+  await i18next.init({
+    lng: navigator.language,
+    debug: true,
+    resources: getResourcesLanguages(resources),
+  });
+};
